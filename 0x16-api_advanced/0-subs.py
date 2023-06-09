46de970d134f6d184548c7cf9ca9181820c18185
Johnfavour <favourjohn851@gmail.com>
2023-06-08 20:56:27 -0700

    A function that queries the Reddit API and returns the number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Contains the number_of_subscribers function
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Request number of subscribers of subreddit
+    """
+    user_agent = '0x16-api_advanced-jmajetich'
+    url = 'https://www.reddit.com/r/{}.json'.format(subreddit)
+
+    headers = {'User-Agent': user_agent}
+
+    r = requests.get(url, headers=headers, allow_redirects=False)
+
+    if r.status_code != 200:
+        return 0
+
+    data = r.json()['data']
+    pages = data['children']
+    page_data = pages[0]['data']
+    return page_data['subreddit_subscribers']
